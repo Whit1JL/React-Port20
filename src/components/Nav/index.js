d9fec67800db046cb757b53537b3c8de5e12d240
Jessica White <jessica2714white@gmail.com>
2021-11-13 05:23:07 -0600

    Created a navbar

---
+++ src/components/Nav/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <ul>
+            <li>
+            <a href="/">Home</a>
+            </li>
+            <li>
+            <a href="/about">About Me</a>
+            </li>
+            <li>
+            <a href="/portfolio">Portfolio</a>
+            </li>
+            <li>
+            <a href="/contact">Contact</a>
+            </li>
+        </ul>
+    );
+};
+
+export default Navbar;
